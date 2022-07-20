06e10bc9d8ab28ebba850deca5e385579b7ad5b8
eephillip <test@example.com>
2022-07-20 20:04:07 +0000

    update

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,10 +1,9 @@
 module "s3_bucket" {
   source = "app.terraform.io/eeforge/s3-bucket/aws"
   version = "2.8.0"
-  bucket = "my-s3-bucket"
+  bucket = "${var.prefix}-my-s3-bucket"
   acl    = "private"
 
-  bucket_prefix = "${var.prefix}"
 
   versioning = {
     enabled = true
